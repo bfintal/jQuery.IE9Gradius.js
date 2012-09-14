f31aaba33f8ca45eea262213a1eee807aa2f92f4
Adam Merrifield <macagp@gmail.com>
2012-09-14 10:42:59 -0400

    Updated scripts to account for element.css('filter') undefined bug in jQuery 1.8

--- jquery.ie9gradius.js
+++ jquery.ie9gradius.js
@@ -1,56 +1,53 @@
-// jquery.ie9gradius.js
-// copyright benjamin intal
-// https://github.com/bfintal/jQuery.IE9Gradius.js
-
 (function($){
 	$.fn.ie9gradius = function(){
-		$(this).each(function(){
+		var that = $(this);
+		for (var i = this.length - 1; i >= 0; i--) {
+			// define filter test
+			var filterscheck = this[i].filters && this[i].filters.length>0;
+
 			// check each css property, we need a border radius and filter
-			if ((parseInt($(this).css('borderTopLeftRadius')) > 0 ||
-				parseInt($(this).css('borderTopRightRadius')) > 0 ||
-				parseInt($(this).css('borderBottomLeftRadius')) > 0 ||
-				parseInt($(this).css('borderBottomRightRadius')) > 0) &&
-				$(this).css('filter') != '' &&
-				$(this).css('filter').match(/DXImageTransform\.Microsoft\.gradient/i) != null) {
+			if ((parseInt($(this[i]).css('borderTopLeftRadius')) > 0 ||
+				parseInt($(this[i]).css('borderTopRightRadius')) > 0 ||
+				parseInt($(this[i]).css('borderBottomLeftRadius')) > 0 ||
+				parseInt($(this[i]).css('borderBottomRightRadius')) > 0) &&
+				filterscheck) {
 
 				// carry over the border radius
-				var s = 'border-top-left-radius: ' + parseInt($(this).css('borderTopLeftRadius')) + 'px;';
-				s += 'border-top-right-radius: ' + parseInt($(this).css('borderTopRightRadius')) + 'px;';
-				s += 'border-bottom-left-radius: ' + parseInt($(this).css('borderBottomLeftRadius')) + 'px;';
-				s += 'border-bottom-right-radius: ' + parseInt($(this).css('borderBottomRightRadius')) + 'px;';
+				var s = 'border-top-left-radius: ' + parseInt($(this[i]).css('borderTopLeftRadius')) + 'px;';
+				s += 'border-top-right-radius: ' + parseInt($(this[i]).css('borderTopRightRadius')) + 'px;';
+				s += 'border-bottom-left-radius: ' + parseInt($(this[i]).css('borderBottomLeftRadius')) + 'px;';
+				s += 'border-bottom-right-radius: ' + parseInt($(this[i]).css('borderBottomRightRadius')) + 'px;';
 
 				// find the start and end colors
-				var c1 = $(this).css('filter').match(/startcolorstr\=\"?\'?\#([0-9a-fA-F]{8})\'?\"?/i);
-				var c2 = $(this).css('filter').match(/endcolorstr\=\"?\'?\#([0-9a-fA-F]{8})\'?\"?/i);
-				if (c1 != null) { if (c1.length == 2) { c1 = c1[1]; } else { c1 = null; } }
-				if (c2 != null) { if (c2.length == 2) { c2 = c2[1]; } else { c2 = null; } }
-				if (c1 == null && c2 != null) { c1 = c2; }
-				else if (c2 == null && c1 != null) { c2 = c1; }
+				thisFilter = this[i].filters.item('DXImageTransform.Microsoft.gradient');
+				var c1 = thisFilter.startColorstr;
+				var c2 = thisFilter.endColorstr;
 
 				// form the filter rule
-				var g = '';
-				if (c1 != null) { var g = 'filter: progid:DXImageTransform.Microsoft.gradient(startColorStr=\'#'+c1+'\', EndColorStr=\'#'+c2+'\');'; }
+				var g = 'filter: progid:DXImageTransform.Microsoft.gradient(startColorStr=\''+c1+'\', endColorStr=\''+c2+'\');';
 
 				var id = 'ie9gradius_'+parseInt(Math.random() * 100000);
 
 				// we need to remove the current filter because this is spilling outside the border radius
+				thisFilter.enabled=0;
+
 				// relative position is needed for proper positioning of the gradient
-				$(this).css('filter', '').css('position', 'relative');
+				$(this[i]).css('position', 'relative');
 
 				// add support for adding hover styling
-				$(this).mouseenter(function() { $('#'+id).addClass('gradiusover'); }).mouseleave(function() { $('#'+id).removeClass('gradiusover'); });
+				$(this[i]).mouseenter(function() { $('#'+id).addClass('gradiusover'); }).mouseleave(function() { $('#'+id).removeClass('gradiusover'); });
 
 				// we need this so that the contents show on top
-				$(this).find('> *:not(ul)').css('position', 'relative');
+				$(this[i]).find('> *:not(ul)').css('position', 'relative');
 
 				// the magic is all here
-				$(this).prepend('\
+				$(this[i]).prepend('\
 				<div style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"> \
 					<div style="'+s+' height: 100%; overflow: hidden;"> \
 						<div id="'+id+'" style="'+g+' height: 100%; width: 100%;"> \
 						</div></div></div>');
 			}
-		});
+		}
 		return $(this);
 	};
 })(jQuery);
